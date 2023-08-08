75def1ec1f261fa88dd699b0256c465ad13a3d3b
maheepatel <mahendarptl9@gmail.com>
2023-08-08 12:14:39 +0530

    deleted backend files

--- Backend/schemas/patents.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose') 
-const schema = mongoose.Schema
-
-const patentSchema = new schema({
-    title:{type:String,required:true},
-    // certificate:{type:String,required:true}
-})
-
-module.exports = mongoose.model('patent',patentSchema)
